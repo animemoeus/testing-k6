3edafa37b5490dad35a83208aaab3708ba4246ae
Arter Tendean <arter.tendean.07@gmail.com>
2025-01-28 22:42:14 +0700

    Update

---
+++ test-node-express.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  vus: 20,
+  duration: "30s",
+};
+
+export default function () {
+  const url = "http://localhost:8014";
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  http.get(url);
+}
